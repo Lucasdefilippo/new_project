01f679f2f8c285a579bc403eebbcb134b9a4c74b
lucas defilippo <lucasdefi00@gmail.com>
2026-07-21 17:17:59 -0300

    feat: add new route post to create Parents

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 import express from "express";
+import { CreateParentsController } from "./src/controllers/Parents/create-parents.js";
 
 const app = express();
 
 app.listen(30000, () => {
-  console.log("running");
+  console.log("O servidor está funcionando na porta 30000");
 });
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Fundionou legal");
+});
+
+app.post("/createParents", async (req, res) => {
+  const createParentsController = new CreateParentsController();
+
+  const { statusCode, result } = await createParentsController.execute(req);
+
+  res.status(statusCode).send(result);
 });
