1c756d862e6eba68fe8c3e0f5f9961a0d2e423c4
lucas defilippo <lucasdefi00@gmail.com>
2026-07-22 11:42:25 -0300

    feat: add route getParent

--- index.js
+++ index.js
@@ -1,22 +1,33 @@
-import express from "express";
-import { CreateParentsController } from "./src/controllers/Parents/create-parents.js";
+import express from 'express'
+import {
+    makeCreateParentsController,
+    makeGetParentsByUserController,
+} from './src/factories/controllers/parents.js'
 
-const app = express();
+const app = express()
 
 app.listen(30000, () => {
-  console.log("O servidor está funcionando na porta 30000");
-});
+    console.log('O servidor está funcionando na porta 30000')
+})
 
-app.use(express.json());
+app.use(express.json())
 
-app.get("/", (req, res) => {
-  res.send("Fundionou legal");
-});
+app.get('/', (req, res) => {
+    res.send('Fundionou legal')
+})
 
-app.post("/createParents", async (req, res) => {
-  const createParentsController = new CreateParentsController();
+app.post('/createParents', async (req, res) => {
+    const createParentsController = makeCreateParentsController()
 
-  const { statusCode, body } = await createParentsController.execute(req);
+    const { statusCode, body } = await createParentsController.execute(req)
 
-  res.status(statusCode).send(body);
-});
+    res.status(statusCode).send(body)
+})
+
+app.get('/getParents', async (req, res) => {
+    const getParentsByUserController = makeGetParentsByUserController()
+
+    const { statusCode, body } = await getParentsByUserController.execute(req)
+
+    res.status(statusCode).send(body)
+})
