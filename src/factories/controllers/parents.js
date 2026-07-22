76d7a2cb4d129f725a605e610ec93b5f97f520a9
lucas defilippo <lucasdefi00@gmail.com>
2026-07-22 11:46:27 -0300

    feat: add factory getParentByUserController

--- src/factories/controllers/parents.js
+++ src/factories/controllers/parents.js
@@ -1,6 +1,9 @@
 import { CreateParentsController } from '../../controllers/Parents/create-parents.js'
+import { GetParentsByUserController } from '../../controllers/Parents/get-parents-by-user.js'
 import { CreateParentsRepository } from '../../repositories/postgres/Parents/PostgresCreateParentsRepository.js'
+import { GetParentsByUserRepository } from '../../repositories/postgres/Parents/PostgresGetParentsByUserRepository.js'
 import { CreateParentsUseCase } from '../../use-cases/Parents/create-parents.js'
+import { GetParentsByUserUseCase } from '../../use-cases/Parents/get-parents-by-user.js'
 
 export const makeCreateParentsController = () => {
     const createParentsRepository = new CreateParentsRepository()
@@ -12,4 +15,16 @@ export const makeCreateParentsController = () => {
     )
 
     return createParentsController
+}
+
+export const makeGetParentsByUserController = () => {
+    const getParentsByUserRepository = new GetParentsByUserRepository()
+    const getParentsByUserUseCase = new GetParentsByUserUseCase(
+        getParentsByUserRepository,
+    )
+    const getParentsByUserController = new GetParentsByUserController(
+        getParentsByUserUseCase,
+    )
+
+    return getParentsByUserController
 }
