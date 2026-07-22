bb8206cfad390a0ead9f0cf48694142b2d974b52
lucas defilippo <lucasdefi00@gmail.com>
2026-07-22 11:45:53 -0300

    feat: add http helpers

--- src/helpers/http.js
+++ src/helpers/http.js
@@ -1,1 +1,3 @@
-export const created = (body) => ({ statusCode: 201, body });
+export const created = (body) => ({ statusCode: 201, body })
+
+export const ok = (body) => ({ statusCode: 200, body })
