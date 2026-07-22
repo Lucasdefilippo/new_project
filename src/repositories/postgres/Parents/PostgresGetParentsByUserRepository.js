ef0caded995ddd9a095ba83a18e9748e5150976b
lucas defilippo <lucasdefi00@gmail.com>
2026-07-22 11:57:51 -0300

    refac: change to findUnique from findFirst

--- src/repositories/postgres/Parents/PostgresGetParentsByUserRepository.js
+++ src/repositories/postgres/Parents/PostgresGetParentsByUserRepository.js
@@ -2,7 +2,7 @@ import { prisma } from '../../../../prisma/prisma.js'
 
 export class GetParentsByUserRepository {
     async execute(user) {
-        return await prisma.parents.findFirst({
+        return await prisma.parents.findUnique({
             where: {
                 user: user,
             },
